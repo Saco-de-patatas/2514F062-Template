1cfd3994e1fff514c290ca2203a75cdb591b2129
Saco-de-patatas <bluepinkmon@gmail.com>
2025-12-02 18:00:11 +0100

    modified:   Examples/NonFreeRTOS/GPIO/GPIO_IOOut_C13/2514F062_it.h 	modified:   Examples/NonFreeRTOS/GPIO/GPIO_IOOut_C13/main.c

--- Examples/NonFreeRTOS/GPIO/GPIO_IOOut_C13/main.c
+++ Examples/NonFreeRTOS/GPIO/GPIO_IOOut_C13/main.c
@@ -4,38 +4,37 @@
 #include "2514F062.h"
 #include "debug.h"
 
+
 void GPIO_Configuration(void)
 {
-    GPIO_InitTypeDef GPIO_InitStructure;
+  GPIO_InitTypeDef GPIO_InitStructure;
 
-    RCC_APB2PeriphClockCmd(RCC_APB2Periph_GPIOC, ENABLE);
-    // PC13
-    GPIO_InitStructure.GPIO_Pin = GPIO_Pin_13;
-    GPIO_InitStructure.GPIO_Speed = GPIO_Speed_50MHz;
-    GPIO_InitStructure.GPIO_Mode = GPIO_Mode_Out_PP;
-    GPIO_Init(GPIOC, &GPIO_InitStructure);
+  RCC_APB2PeriphClockCmd(RCC_APB2Periph_GPIOC, ENABLE);
+  GPIO_InitStructure.GPIO_Pin = GPIO_Pin_13;
+  GPIO_InitStructure.GPIO_Speed = GPIO_Speed_50MHz;
+  GPIO_InitStructure.GPIO_Mode = GPIO_Mode_Out_PP;
+  GPIO_Init(GPIOC, &GPIO_InitStructure);
 }
 
 int main(void)
 {
-    RCC_ClocksTypeDef clocks;
-    Delay_Init();
-    USART_Printf_Init(115200);
-    RCC_GetClocksFreq(&clocks);
+  RCC_ClocksTypeDef clocks;
+  Delay_Init();
+  USART_Printf_Init(115200);
+  RCC_GetClocksFreq(&clocks);
 
-    printf("\n");
-    printf("SYSCLK: %3.1fMhz, HCLK: %3.1fMhz, PCLK1: %3.1fMhz, PCLK2: %3.1fMhz, ADCCLK: %3.1fMhz\n",
-           (float)clocks.SYSCLK_Frequency / 1000000, (float)clocks.HCLK_Frequency / 1000000,
-           (float)clocks.PCLK1_Frequency / 1000000, (float)clocks.PCLK2_Frequency / 1000000, 
-		   (float)clocks.ADCCLK_Frequency / 1000000);
-    printf("2514F062 GPIO Output Test\n");
-    GPIO_Configuration();
+  printf("\n");
+  printf("SYSCLK: %ld, HCLK: %ld, PCLK1: %ld, PCLK2: %ld, ADCCLK: %ld\n",
+         clocks.SYSCLK_Frequency, clocks.HCLK_Frequency,
+         clocks.PCLK1_Frequency, clocks.PCLK2_Frequency, clocks.ADCCLK_Frequency);
+  printf("El CheapoPill GPIO Output Test.\n");
+  GPIO_Configuration();
 
-    while (1)
-    {
-        GPIO_SetBits(GPIOC, GPIO_Pin_13);
-        Delay_Ms(500);
-        GPIO_ResetBits(GPIOC, GPIO_Pin_13);
-        Delay_Ms(500);
-    }
+  while (1)
+  {
+    GPIO_SetBits(GPIOC, GPIO_Pin_13);
+    Delay_Ms(200);
+    GPIO_ResetBits(GPIOC, GPIO_Pin_13);
+    Delay_Ms(200);
+  }
 }
